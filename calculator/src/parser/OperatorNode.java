36a37aa61d1dfae1184c8f028388c99eb1fcc238
alxv05 <alexander.kai.chen@gmail.com>
2023-02-14 19:10:40 -0600

    set OperatorNode left & right to final

--- calculator/src/parser/OperatorNode.java
+++ calculator/src/parser/OperatorNode.java
@@ -1,19 +1,11 @@
 package parser;
 
 public abstract class OperatorNode implements AstNode {
-    protected AstNode left;
-    protected AstNode right;
+    protected final AstNode left;
+    protected final AstNode right;
 
     public OperatorNode(AstNode left, AstNode right) {
         this.left = left;
-        this.right = right;
-    }
-
-    public void setLeft(AstNode left) {
-        this.left = left;
-    }
-
-    public void setRight(AstNode right) {
         this.right = right;
     }
 }
